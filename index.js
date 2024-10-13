316d7fe155ef4d0b89bc92017fb12776b70d2f28
raghavbhaiji <rg378875@gmail.com>
2024-10-13 13:55:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,8 +109,8 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Other bots`, type: ActivityType.Competing }],
-      status: 'available',
+      activities: [{ name: `RG DEVELOPEMENT VOUCHES`, type: ActivityType.watching }],
+      status: 'DND',
     });
   }, 2000); 
 });
